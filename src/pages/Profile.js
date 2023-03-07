350f58ff9e280b9891f3a2311a611b5690edcf5b
Georgia <georgiab543@gmail.com>
2023-03-07 17:38:31 -0300

    requisitos profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,50 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const history = useHistory();
+  const emailLogin = JSON.parse(localStorage.getItem('user'));
+
+  const redirectDoneRecipes = () => {
+    history.push('/done-recipes');
+  };
+
+  const redirectFavorites = () => {
+    history.push('/favorite-recipes');
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
       <Header title="Profile" printIcon={ false } />
-      <h1>Test Profile</h1>
+      <p data-testid="profile-email">{ emailLogin.email }</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ redirectDoneRecipes }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ redirectFavorites }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ handleLogout }
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
