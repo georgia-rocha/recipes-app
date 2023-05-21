615fdd8a76c04cecd310bd1905cde6e1f674cb9a
Georgia <georgiab543@gmail.com>
2023-05-21 02:05:45 -0300

    Updates

--- src/pages/RecipeInProgress.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import RecipeDetails from './RecipeDetails';
-
-// Página de receita em progresso
-export default function RecipeInProgress() {
-  return <RecipeDetails isRecipeStarted />;
-}
